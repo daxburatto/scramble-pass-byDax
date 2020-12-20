201ce3dd49460226e9a42c365d8e36d988ee1133
Dax Buratto <daxburatto@gmail.com>
2020-12-20 16:38:22 -0600

    added options to html for parameters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,18 @@
 // Assignment code here
+var randomNumber = function (min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min)
 
+   return value
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
+
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword()
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
