4798df2ab1c9bda92b543ce7e9d46e5e040665f5
Dax Buratto <daxburatto@gmail.com>
2020-12-20 23:40:50 -0600

    added checkbox options, tried to get code to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,79 @@
 // Assignment code here
-var randomNumber = function (min, max) {
-  var value = Math.floor(Math.random() * (max - min + 1) + min)
 
-   return value
+var answer = document.getElementById("#password")
+var passLength = document.getElementById("#length")
+var upperCase = document.getElementById("#upper")
+var lowerCase = document.getElementById("#lower")
+var number = document.getElementById("#numbers")
+var symbol = document.getElementById("#special")
+var generateBtn = document.querySelector("#generate")
+
+const randomFunc = {
+  upper : getRandomUpperCase,
+  lower : getRandomLowerCase,
+  number : getRandomNumber,
+  symbol : getRandomSymbol
 }
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener('click', () => {
+  const passLength = +length.value
+  const hasUpper = ('upper').checked
+  const hasLower = ('lower').checked
+  const hasNumber = ('numbers').checked
+  const hasSymbol = ('special').checked
 
-// Write password to the #password input
+  answer = generatePassword(hasUpper, hasLower, hasNumber, hasSymbol, length)
+})
 
+function generatePassword(upper, lower, numbers, special, length) {
+  var generatePassword = ''
+  const typesCount = upper + lower + numbers + special
+  console.log(typesCount)
+  const typesArr = [{upper}, {lower}, {numbers}, {special}].filter(item => Object.values(item)[0])
+  if (typesCount === 0) {
+    return ''
+  } 
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach(type => {
+      const funcName = Object.keys(type)[0]
+      generatePassword += randomFunc[funcName]()
+    })
+  }
+  const finalPassword = generatePassword.slice(0, length)
+  return finalPassword
+}
+
+function getRandomUpperCase() {
+  return String.fromCharCode(Math.floor(Math.random()*26)+65)
+}
+
+function getRandomLowerCase() {
+  return String.fromCharCode(Math.floor(Math.random()*26)+97)
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random()*10)+48)
+}
+
+function getRandomSymbol() {
+  var symbol = "!@#$%^&*()=<>/,.|~?"
+  return symbol[Math.floor(Math.random()*symbol.length)]
+}
+
+let me = document.getElementById("length").value
+
+var btn = document.getElementById("generate")
+generate.addEventListener('click', () => {
+  console.log(me)
+})
 
 function writePassword() {
   var password = generatePassword()
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector('#password')
 
-  passwordText.value = password;
-
+  passwordText.value = password
+  debugger
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener = ('click', writePassword)
+console.log(generatePassword())
